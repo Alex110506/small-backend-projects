a263d6a2dd0cf2bc3d0f6b0944a086d40bdace70
Alexandru Rădulescu <alex11amr110506@gmail.com>
2025-09-07 01:39:32 +0300

    local functionality done

---
+++ file-upload/controllers/uploadsController.js
@@ -0,0 +1,28 @@
+const path=require("path")
+const {StatusCodes}=require("http-status-codes")
+const CustomError=require("../errors")
+
+const uploadProductImage=async (req,res)=>{
+    if(!req.files){
+        throw new CustomError.BadRequestError("No file uploaded!")
+    }
+    const productImage=req.files.image
+
+    if(!productImage.mimetype.startsWith("image")){
+        throw new CustomError.BadRequestError("Please upload Image")
+    }
+
+    const maxSize=1024*1024
+    if(productImage.size>maxSize){
+        throw new CustomError.BadRequestError("Please upload image smaller than 1kb")
+    }
+
+    const imagePath=path.join(__dirname,"../public/uploads/"+`${productImage.name}`)
+    await productImage.mv(imagePath)
+
+    res.status(StatusCodes.OK).json({image:{src:`/uploads/${productImage.name}`}})
+}
+
+module.exports={
+    uploadProductImage
+}
